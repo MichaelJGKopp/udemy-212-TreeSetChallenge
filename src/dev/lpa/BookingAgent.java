7216c56d14267d4fafc3e2aac9fc092f0197250e
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-06 17:33:31 +0200

    instantiate theatre in main and print seating

--- src/dev/lpa/BookingAgent.java
+++ src/dev/lpa/BookingAgent.java
@@ -4,6 +4,10 @@ public class BookingAgent {
 
   public static void main(String[] args) {
 
+    int rows = 10;
+    int totalSeats = 100;
 
+    Theatre theatre = new Theatre("Richard Rodgers", rows, totalSeats);
+    theatre.printSeatMap();
   }
 }
