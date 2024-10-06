aed5eb7ccc980e6b5ea0bdc16293349745271339
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-06 20:16:29 +0200

    showcase more testing of bookSeats(...)

--- src/dev/lpa/BookingAgent.java
+++ src/dev/lpa/BookingAgent.java
@@ -18,6 +18,12 @@ public class BookingAgent {
     bookSeat(theatre, 'M', 1);
 
     bookSeats(theatre, 4, 'B', 3, 10); // booking contiguous seats
+    bookSeats(theatre, 6, 'B', 'C', 3, 10);
+    bookSeats(theatre, 4, 'B', 1, 10);
+    bookSeats(theatre, 4, 'B', 'C', 1, 10);
+    bookSeats(theatre, 1, 'B', 'C', 1, 10);
+    bookSeats(theatre, 4, 'M', 'Z', 1, 10);
+    bookSeats(theatre, 10, 'A', 'E', 1, 10);
   }
 
   private static void bookSeat(Theatre theatre, char row, int seatNo) {
