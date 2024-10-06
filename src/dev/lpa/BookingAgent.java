f2db15a87f9f041a7e0d97406ff2c67644878f1b
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-06 17:53:02 +0200

    reserve a seat twice in main

--- src/dev/lpa/BookingAgent.java
+++ src/dev/lpa/BookingAgent.java
@@ -9,5 +9,20 @@ public class BookingAgent {
 
     Theatre theatre = new Theatre("Richard Rodgers", rows, totalSeats);
     theatre.printSeatMap();
+
+    bookSeat(theatre, 'A', 3);
+    bookSeat(theatre, 'A', 3);
   }
+
+  private static void bookSeat(Theatre theatre, char row, int seatNo) {
+
+    String seat = theatre.reserveSeat(row, seatNo);
+    if (seat != null) {
+      System.out.println("Congratulations! Your reserved seat is " + seat);
+      theatre.printSeatMap();
+    } else {
+      System.out.println("Sorry! Unable to reserve " + row + seatNo);
+    }
+  }
+
 }
